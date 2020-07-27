f3f5eecf998fa9259a682bd26329b27289c10424
morgpion <morg.leen@ouvaton.org>
2020-07-27 19:43:52 +0200

    déplacement depuis gitlab où il est en privé

---
+++ application/helpers/onglet_title_helper.php
@@ -0,0 +1,17 @@
+<?php
+
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+if ( ! function_exists('title')) {
+	/**
+	 * Display website title
+	 *
+	 */
+    function title($page_title = '', $site_title = 'Anciens élèves de BeWeb') {
+
+        return $page_title.' | '.$site_title;
+    }
+    
+}
+
+
